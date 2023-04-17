bf894cf10e7975ae1e4611e81765646dcbbc991e
Harleen <61685399+harleen7@users.noreply.github.com>
2023-04-18 00:01:38 +0530

    Update html.js

--- src/html.js
+++ src/html.js
@@ -11,7 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-            <script
+        <script
           dangerouslySetInnerHTML={{
             __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -19,7 +19,9 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-P38SHHG');
-    `,
+            `,
+          }}
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
